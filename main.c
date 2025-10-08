e42f81b1cc88889432c2ec57bead8e289d4f90d8
Pablo Romero <romeropablojr03@gmail.com>
2025-10-07 19:34:01 -0500

    refactor to C

---
+++ main.c
@@ -0,0 +1,149 @@
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+struct VkState{
+    struct Resolution{
+        uint32_t width;
+        uint32_t height;
+    }resolution;
+    GLFWwindow* window;
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+};
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+        VkDebugUtilsMessageTypeFlagsEXT messageType, 
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
+        void* pUserData){
+    printf("validation layer: %s \n", pCallbackData->pMessage);
+
+    return VK_FALSE;
+}
+
+void setupDebugMessenger(){
+    VkDebugUtilsMessengerCreateInfoEXT createInfo = {
+        .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+
+        .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+
+        .messageType =  VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | 
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | 
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+        .pfnUserCallback = debugCallback,
+        .pUserData = NULL
+    };
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, 
+        const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, 
+        const VkAllocationCallbacks* pAllocator, 
+        VkDebugUtilsMessengerEXT* pDebugMessenger){
+
+}
+
+bool checkValidationLayerSupport(){
+    const char* validationLayer = "VK_LAYER_KHRONOS_validation";
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, NULL);
+    VkLayerProperties availableLayers[layerCount];
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers);
+
+    bool layerFound = false;
+    for(uint32_t i = 0; i < layerCount; i++){
+        if(strcmp(availableLayers[i].layerName, validationLayer) == 0){
+            printf("[+] Found validation layer %s \n", validationLayer);
+            layerFound = true;
+            break;
+        }
+    }
+
+    return layerFound;
+}
+
+void initWindow(struct VkState* state){
+    glfwInit();
+    state->resolution.width  = 800;
+    state->resolution.height = 600;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    state->window = glfwCreateWindow(state->resolution.width, state->resolution.height, "Vulkan", NULL, NULL);
+    assert(state->window);
+}
+
+void createInstance(struct VkState* state){
+    assert(checkValidationLayerSupport);
+    VkApplicationInfo appInfo = {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Vulkan",
+        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "null",
+        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = VK_API_VERSION_1_0
+    };
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
+    VkExtensionProperties extensions[extensionCount];
+    vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, extensions);
+    for(uint32_t i = 0; i < extensionCount; i++){
+        printf("[!] Found: %s \n", extensions[i].extensionName);
+    }
+
+    uint32_t glfwExtensionsCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionsCount);
+    uint32_t enabledExtensionCount = glfwExtensionsCount + 1;
+    const char* enabledExtensions[enabledExtensionCount];
+    memcpy(enabledExtensions, glfwExtensions, sizeof(char*) * glfwExtensionsCount);
+    enabledExtensions[glfwExtensionsCount] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+
+    uint32_t validationLayerCount = 1;
+    const char * validationLayers[validationLayerCount];
+    validationLayers[0] = "VK_LAYER_KHRONOS_validation";
+
+    VkInstanceCreateInfo createInfo = {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &appInfo,
+        .enabledExtensionCount = enabledExtensionCount,
+        .ppEnabledExtensionNames = enabledExtensions,
+        .enabledLayerCount = validationLayerCount,
+        .ppEnabledLayerNames = validationLayers
+    };
+
+    VkResult result = vkCreateInstance(&createInfo, NULL, &state->instance);
+    assert(result == VK_SUCCESS);
+}
+
+void renderLoop(struct VkState* state){
+    while(!glfwWindowShouldClose(state->window)){
+        glfwPollEvents();
+    }
+}
+
+void cleanup(struct VkState* state){
+    vkDestroyInstance(state->instance, NULL);
+
+    glfwDestroyWindow(state->window);
+
+    glfwTerminate();
+}
+
+int main(void){
+    printf("[+] Running Vulkan Program\n");
+    struct VkState state;
+    memset(&state, 0, sizeof(state));
+
+    initWindow(&state);
+    createInstance(&state);
+
+    renderLoop(&state);
+    cleanup(&state);
+}
